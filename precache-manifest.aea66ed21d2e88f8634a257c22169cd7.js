6f6cd48ada92bf25e4fe9bb32f73d6b5332e2a78
Kexin Lu <kexin.lu@hcareers.com>
2020-07-21 22:41:32 -0700

    new wfadd

---
+++ precache-manifest.aea66ed21d2e88f8634a257c22169cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120899760b7c0b9e4272fb468df42308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7fb39f09131794e5b01",
+    "url": "/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "01c9dc000a7118a5b7d3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7fb39f09131794e5b01",
+    "url": "/static/js/2.540b392e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.540b392e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c9dc000a7118a5b7d3",
+    "url": "/static/js/main.c8dc8d04.chunk.js"
+  },
+  {
+    "revision": "d93c559b28956c57f48a",
+    "url": "/static/js/runtime-main.0bf19274.js"
+  }
+]);
